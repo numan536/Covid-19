068723f5cf8f3fbe63902dba5790464fe71e70f4
numan536 <numansaeed536@gmail.com>
2020-04-02 11:43:17 -0700

    Production Build Created

---
+++ build/precache-manifest.35e716f763e0f86550e962fa02f7d507.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a829a6be881f84a7ec6f495889604251",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ee738b5fc3e13004e1c6",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "5e5f1b25d9024ee34cd8",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "ee738b5fc3e13004e1c6",
+    "url": "/static/js/2.30e64a3e.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.30e64a3e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5e5f1b25d9024ee34cd8",
+    "url": "/static/js/main.326b2745.chunk.js"
+  },
+  {
+    "revision": "94a45316d015e52eacc6",
+    "url": "/static/js/runtime-main.60cbd1db.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const path = require('path')
+
+const app = express();
+
+app.use(express.static('./build'))
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './build/index.html'))
+})
+
+app.listen(process.env.PORT || 4000, () => {
+    console.log('port is listening on port 4000')
+})
